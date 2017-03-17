30a3133a8482139ff94e3055ef21fc47647951ad
zry731134543 <731134543@qq.com>
2017-03-17 17:54:44 +0800

    Signed-off-by: zry731134543 <731134543@qq.com>

--- src/com/ryzhang/downloaddemo/MainActivity.java
+++ src/com/ryzhang/downloaddemo/MainActivity.java
@@ -4,12 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
-
+	WebView webView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
+		webView=(WebView)findViewById(R.id.web);
+		webView.loadUrl("file:///android_asset/ife/layout.html");
+		
 	}
 }
